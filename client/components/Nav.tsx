d8106f45b64c5318febaddc77adcce1b167673f9
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-09 23:14:34 +1200

    add authentication

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,6 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { Button } from '@headlessui/react'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 export default function Nav() {
+  const { logout, loginWithRedirect } = useAuth0()
+
+  const handleSignOut = () => {
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
   return (
     <div className="flex justify-between m-3">
       <h1 className="gap-2 py-1.5 px-3 text-xl font-semibold text-white">
@@ -15,9 +27,22 @@ export default function Nav() {
             My Tasks
           </li>
         </ul>
-        <Button className="rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
-          Sign In
-        </Button>
+        <IfAuthenticated>
+          <Button
+            onClick={handleSignOut}
+            className="rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+          >
+            Sign Out
+          </Button>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <Button
+            onClick={handleSignIn}
+            className="rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+          >
+            Sign In
+          </Button>
+        </IfNotAuthenticated>
       </div>
     </div>
   )
--- client/index.tsx
+++ client/index.tsx
@@ -3,6 +3,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { routes } from './routes'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const router = createBrowserRouter(routes)
 
@@ -10,9 +11,18 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>,
+    <Auth0Provider
+      domain="dev-mako-2024-olivia.au.auth0.com"
+      clientId="F5eiaLpVKWLNrdRTznkMDU1LQD7V6iWZ"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: 'https://todos/api',
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Auth0Provider>,
   )
 })
