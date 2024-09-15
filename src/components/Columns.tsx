72a0f4ddf1bd9ba3f152159cf2d1fb6638be61fb
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-15 19:31:25 +1200

    added dropdown for prioity

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,5 +1,16 @@
 import { ColumnDef } from '@tanstack/react-table'
 import { TodosData } from 'models/todos'
+import { MoreHorizontal } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 export const columns: ColumnDef<TodosData>[] = [
   {
@@ -13,6 +24,29 @@ export const columns: ColumnDef<TodosData>[] = [
   {
     accessorKey: 'priority',
     header: 'Priority',
+    cell: ({ row }) => {
+      const priority = row.getValue('priority')
+      return (
+        <div className="flex justify-between items-center">
+          <div>{priority}</div>
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>low</DropdownMenuItem>
+                <DropdownMenuItem>medium</DropdownMenuItem>
+                <DropdownMenuItem>high</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      )
+    },
   },
   {
     accessorKey: 'dueDate',
