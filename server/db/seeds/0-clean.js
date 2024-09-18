ff4e4ce814c1f975b057bc92a8b2a529a81d8cb7
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-19 11:09:48 +1200

    remove users table

--- server/db/seeds/0-clean.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-export const seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex('todos').del()
-  await knex('users').del()
-}
