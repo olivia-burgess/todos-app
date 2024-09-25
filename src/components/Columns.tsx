ddeb1b8a46ec6e77e8b38e34439eca3648bfb7fd
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-25 21:29:24 +1200

    add sort icon

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -6,6 +6,7 @@ import DueDateCell from './DueDateCell'
 import CategoryCell from './CategoryCell'
 import DeleteCell from './DeleteCell'
 import CompleteCell from './CompleteCell'
+import SortIcon from './SortIcon'
 
 export const columns: ColumnDef<TodosData>[] = [
   {
@@ -19,17 +20,17 @@ export const columns: ColumnDef<TodosData>[] = [
   },
   {
     accessorKey: 'category',
-    header: 'Category',
+    header: () => <SortIcon columnName={'Category'} />,
     cell: CategoryCell,
   },
   {
     accessorKey: 'priority',
-    header: 'Priority',
+    header: () => <SortIcon columnName={'Priority'} />,
     cell: PriorityCell,
   },
   {
     accessorKey: 'dueDate',
-    header: 'Due Date',
+    header: () => <SortIcon columnName={'Due Date'} />,
     cell: DueDateCell,
   },
   {
